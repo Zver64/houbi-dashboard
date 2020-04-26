34319164542a16c227c41665636c05bc5ae0c578
Zver64 <coolvill@mail.ru>
2020-04-26 23:06:41 +0300

    Updates

---
+++ precache-manifest.0021e23e492a286de64a612b1dcaacc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef866acca0f2bd311acbc81f8390fc8",
+    "url": "/houbi-dashboard/index.html"
+  },
+  {
+    "revision": "3d97fbe2e3a5fdef83c9",
+    "url": "/houbi-dashboard/static/css/main.8afb962f.chunk.css"
+  },
+  {
+    "revision": "ebbf3adc968036d9e235",
+    "url": "/houbi-dashboard/static/js/2.1bddf17b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/houbi-dashboard/static/js/2.1bddf17b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d97fbe2e3a5fdef83c9",
+    "url": "/houbi-dashboard/static/js/main.dc56e41f.chunk.js"
+  },
+  {
+    "revision": "bdea6d5251b49de27e13",
+    "url": "/houbi-dashboard/static/js/runtime-main.ec15eee0.js"
+  }
+]);
