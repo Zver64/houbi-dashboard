4327b64799c22dc9802656adafa28be25973aa42
Zver64 <coolvill@mail.ru>
2020-04-26 23:06:11 +0300

    setted up github pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,45 @@ import React from 'react'
 import styled from 'styled-components'
 function App({ className }: { className?: string }) {
   return (
-    <div className='App'>
-      <header className={className}>This is the header of the future</header>
+    <div className={className}>
+      <header>
+        <nav>
+          <ul>
+            <li>link1</li>
+            <li>link2</li>
+            <li>link3</li>
+            <li>link4</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>This is the header of the future</h1>
+      </main>
     </div>
   )
 }
 
 export default styled(App)`
-  background-color: var(--rgb-color3);
+  width: 1200px;
+  max-width: 100%;
+  header {
+    padding: 0.5em 0;
+  }
+  nav {
+    ul {
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+      li {
+        padding: 1em;
+        border: 1px solid var(--rgb-color2);
+      }
+    }
+  }
+  main {
+    border: 1px solid var(--rgb-color2);
+  }
+  h1 {
+    text-align: center;
+  }
 `
