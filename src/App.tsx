d75d98ba94ee3f7b8689ce2245af41964adcfb14
Zver64 <coolvill@mail.ru>
2020-04-27 11:49:56 +0300

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,61 @@
 import React from 'react'
 import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 function App({ className }: { className?: string }) {
   return (
-    <div className={className}>
-      <header>
-        <nav>
-          <ul>
-            <li>link1</li>
-            <li>link2</li>
-            <li>link3</li>
-            <li>link4</li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <h1>This is the header of the future</h1>
-      </main>
-    </div>
+    <Router>
+      <div className={className}>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/link1">link1</Link>
+              </li>
+              <li>
+                <Link to="/link2">link2</Link>
+              </li>
+              <li>
+                <Link to="/link3">link3</Link>
+              </li>
+              <li>
+                <Link to="/link4">link4</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <h1>This is the header of the future</h1>
+          <Switch>
+            <Route path="/link1">
+              <div>This is link 1</div>
+            </Route>
+            <Route path="/link2">
+              <div>This is link 2</div>
+            </Route>
+            <Route path="/link3">
+              <div>This is link 3</div>
+            </Route>
+            <Route path="/link4">
+              <div>This is link 4</div>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   )
 }
 
 export default styled(App)`
-  width: 1200px;
-  max-width: 100%;
   header {
     padding: 0.5em 0;
+    width: 1200px;
+    max-width: 100%;
+    margin: 0 auto;
   }
   nav {
     ul {
@@ -38,7 +69,11 @@ export default styled(App)`
     }
   }
   main {
-    border: 1px solid var(--rgb-color2);
+    width: 1200px;
+    max-width: 100%;
+    margin: 0 auto;
+    background-color: var(--rgb-color1);
+    padding: 1em;
   }
   h1 {
     text-align: center;
