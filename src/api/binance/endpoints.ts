976ccd13ccf05b93e7e3ec893bea999f57a69ac7
Zver64 <coolvill@mail.ru>
2020-05-02 15:08:24 +0300

    added endpoints

--- src/api/binance/endpoints.ts
+++ src/api/binance/endpoints.ts
@@ -1,9 +1,19 @@
 export const API_CALLS = {
-  PING: 'ping'
+  PING: 'ping',
+  ACCOUNT: 'account',
+  ORDER: 'order'
 }
 export default {
   [API_CALLS.PING]: {
     private: false,
     path: '/api/v3/ping'
+  },
+  [API_CALLS.ACCOUNT] : {
+    private: true,
+    path: '/api/v3/account'
+  },
+  [API_CALLS.ORDER]: {
+    private: true,
+    path: '/api/v3/order'
   }
 }
